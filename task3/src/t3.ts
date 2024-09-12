93ae03a8909796d809dbf09e2bf103391c93ea92
Azzni96 <nihadazzam96@gmail.com>
2024-09-12 13:28:10 +0300

    update

---
+++ task3/src/t3.ts
@@ -0,0 +1,24 @@
+export {}; // hack to ignore Book from task 4
+// TODO: Define a type alias named 'Book' with appropriate properties
+
+
+// TODO: Implement the promptForBook function
+function promptForBook() {
+    // TODO: Prompt user for book details (title, author, publication year)
+    const bookTitle = prompt("Enter book title") /* TODO: Get user input for title */;
+    const bookAuthor = prompt("Enter book Author")/* TODO: Get user input for author */;
+    const bookPublicationYear = prompt("Enter book PublicationYear")/* TODO: Get user input for publication year */;
+
+    // TODO: Create an object of type 'Book' with the entered values
+    const book: Book =  /* TODO: Create the Book object */;
+    return book;
+}
+
+// TODO: Call the promptForBook function to get the book details
+const bookDetails = promptForBook();
+
+// TODO: Display the details of the book object to the user
+console.log("Book Details:");
+console.log(`Title: ${bookDetails.title}`);
+console.log(`Author: ${bookDetails.author}`);
+console.log(`Publication Year: ${bookDetails.publicationYear}`);
